807d9d0c1dc2d5e41759181d192db98aabe439aa
Omar Tayel <omar2tayel@gmail.com>
2015-03-18 00:12:10 +0200

    Stack of Plates

--- Chapter3CSharp/Program.cs
+++ Chapter3CSharp/Program.cs
@@ -10,6 +10,11 @@ namespace Chapter3CSharp
     {
         static void Main(string[] args)
         {
+            TestStackOfPlates();
+        }
+
+        static void TestThreeStacks()
+        {
             ThreeStacks<int> threeStacks = new ThreeStacks<int>();
             threeStacks.Push(0, 1);
             threeStacks.Push(0, 2);
@@ -20,6 +25,36 @@ namespace Chapter3CSharp
             threeStacks.Push(0, 6);
             threeStacks.Push(1, 7);
             threeStacks.Push(1, 8);
+        }
+
+        static void TestStackOfPlates()
+        {
+            StackOfPlates<int> stack = new StackOfPlates<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            stack.Push(10);
+
+            Console.WriteLine(stack.PopAt(0));
+            Console.WriteLine(stack.PopAt(0));
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.Push(11);
+            stack.Push(12);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ Chapter3CSharp/StackOfPlates.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3CSharp
+{
+    class StackOfPlates<T>
+    {
+        const int STACK_CAPACITY = 2;
+        List<Stack<T>> stacks;
+        int currentStack;
+
+        public StackOfPlates()
+        {
+            stacks = new List<Stack<T>>();
+            stacks.Add(new Stack<T>(STACK_CAPACITY));
+            currentStack = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (currentStack < 0)
+            {
+                Console.WriteLine("No Stacks - Initializing again.");
+                stacks.Add(new Stack<T>(STACK_CAPACITY));
+                currentStack = 0;
+            }
+
+            if (stacks[currentStack].Count == STACK_CAPACITY)
+            {
+                Console.WriteLine("Stack Full - Adding new Stack.");
+                currentStack++;
+                stacks.Add(new Stack<T>(STACK_CAPACITY));
+            }
+
+            stacks[currentStack].Push(item);
+        }
+
+        public T Pop()
+        {
+            if (currentStack < 0 || stacks[currentStack].Count == 0)
+            {
+                throw new Exception("Empty Stack");
+            }
+
+            T item = stacks[currentStack].Pop();
+            if (stacks[currentStack].Count == 0)
+            {
+                stacks.RemoveAt(currentStack);
+                currentStack--;
+            }
+
+            return item;
+        }
+
+        public T PopAt(int index)
+        {
+            if (index > currentStack)
+            {
+                throw new Exception("Index is out of range");
+            }
+
+            T item = stacks[index].Pop();
+            if (stacks[index].Count == 0)
+            {
+                stacks.RemoveAt(index);
+                currentStack--;
+            }
+            else
+            {
+                Shift(index + 1);
+            }
+
+            return item;
+        }
+
+        private void Shift(int index)
+        {
+            while (index <= currentStack)
+            {
+                Stack<T> tempStack = new Stack<T>();
+                while (stacks[index].Count > 0)
+                {
+                    tempStack.Push(stacks[index].Pop());
+                }
+
+                stacks[index - 1].Push(tempStack.Pop());
+
+                while (tempStack.Count > 0)
+                {
+                    stacks[index].Push(tempStack.Pop());
+                }
+
+                index++;
+            }
+
+            if (stacks[currentStack].Count == 0)
+            {
+                stacks.RemoveAt(currentStack);
+                currentStack--;
+            }
+        }
+
+    }
+}
