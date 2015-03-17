c9516cb19d493f5a829144c368a8754877059906
Omar Tayel <omar2tayel@gmail.com>
2015-03-18 01:54:51 +0200

    Check if there is route between two nodes in graph

---
+++ Chapter4CSharp/4.2 GraphRoute.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4CSharp
+{
+    class GraphRoute
+    {
+        public bool CheckIfPathExists(GraphNode<int> sourceNode, GraphNode<int> destinationNode)
+        {
+            Queue<GraphNode<int>> queue = new Queue<GraphNode<int>>();
+
+            if (sourceNode == null || destinationNode == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (sourceNode.Value == destinationNode.Value)
+                {
+                    return true;
+                }
+                else
+                {
+                    sourceNode.Visited = true;
+                    queue.Enqueue(sourceNode);
+                    while (queue.Count > 0)
+                    {
+                        var node = queue.Dequeue();
+                        foreach (GraphNode<int> neighbor in node.Nodes)
+                        {
+                            if (!neighbor.Visited)
+                            {
+                                if (neighbor.Value == destinationNode.Value)
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    neighbor.Visited = true;
+                                    queue.Enqueue(neighbor);
+                                }
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+    }
+
+    class GraphNode<T>
+    {
+        public List<GraphNode<T>> Nodes { get { return nodes; } }
+        public T Value { get { return this.value; } }
+
+        public bool Visited { set { this.visited = value; } get { return this.visited; } }
+
+        T value;
+        List<GraphNode<T>> nodes;
+        bool visited;
+
+        public GraphNode(T value)
+        {
+            this.value = value;
+            this.visited = false;
+            nodes = new List<GraphNode<T>>();
+        }
+
+        public void AddNode(GraphNode<T> node)
+        {
+            this.nodes.Add(node);
+        }
+
+
+    }
+}
--- Chapter4CSharp/Program.cs
+++ Chapter4CSharp/Program.cs
@@ -10,12 +10,41 @@ namespace Chapter4CSharp
     {
         static void Main(string[] args)
         {
-            TestBalancedTree();
+            TestGraphRoute();
         }
 
         static void TestBalancedTree()
         {
             CheckBalancedTree balancedTree = new CheckBalancedTree();
+        }
+
+        static void TestGraphRoute()
+        {
+            GraphNode<int> node8 = new GraphNode<int>(8);
+            GraphNode<int> node4 = new GraphNode<int>(4);
+            node4.AddNode(node8);
+
+            GraphNode<int> node3 = new GraphNode<int>(3);
+            node3.AddNode(node4);
+
+            GraphNode<int> node5 = new GraphNode<int>(5);
+            node5.AddNode(node4);
+
+            GraphNode<int> node6 = new GraphNode<int>(6);
+            node6.AddNode(node5);
+
+            GraphNode<int> node2 = new GraphNode<int>(2);
+            //node2.AddNode(node3);
+            node2.AddNode(node6);
+
+            GraphNode<int> node7 = new GraphNode<int>(7);
+            node7.AddNode(node5);
+
+            GraphNode<int> node1 = new GraphNode<int>(1);
+            node1.AddNode(node2);
+            node1.AddNode(node7);
+
+            Console.WriteLine(new GraphRoute().CheckIfPathExists(node1, node3));
         }
     }
 }
